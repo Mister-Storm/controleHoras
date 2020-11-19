c5886e9030a1ec005af455e7c2cab4d720f2fd7a
Fernando Costa Leite <clfernando1979@hotmail.com>
2020-11-19 00:44:15 -0300

    fix bug authorization

--- src/main/java/com/fernando/zallpy/controlehoras/security/JWTAuthorizationFilter.java
+++ src/main/java/com/fernando/zallpy/controlehoras/security/JWTAuthorizationFilter.java
@@ -33,8 +33,8 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
             if(auth != null) {
                 SecurityContextHolder.getContext().setAuthentication(auth);
             }
-            chain.doFilter(request, response);
         }
+        chain.doFilter(request, response);
     }
 
     private UsernamePasswordAuthenticationToken getAuthentication(String token) {
